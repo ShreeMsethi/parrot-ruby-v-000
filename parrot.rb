156f5d0a8de0084bd56a7b95d64a6cd30eb43ce0
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-02-13 21:15:22 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot ("Hello, I am parrot.", sound = "Squawk!")
-  puts "Hello, I am parrot."
-  return "Hello, I am parrot."
+def parrot (name,sound = "Squawk!")
+  puts "Hello my name is #{name} and i love to #{sound}."
+  return "Hello my name is #{name} and i love to #{sound}."
 end
   
-parrot("Hello, I am parrot.")
+parrot(Mithu)
