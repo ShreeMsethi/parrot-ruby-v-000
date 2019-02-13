62e31715761fdf1047e202304eada11fa9c4f1da
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-02-13 21:21:36 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot (sound = "Squawk!")
-  puts "Hello my name is Mithu and i love to #{sound}."
-  return "Hello my name is Mithu and i love to #{sound}."
+  puts 
+  return 
 end
   
 parrot
